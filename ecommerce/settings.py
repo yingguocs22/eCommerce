5312eace3cb7dd4b933b6556c249106768cecc6b
yingguocs22 <guoyingcs@163.com>
2023-03-23 14:13:00 -0700

    updata databse

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -75,10 +75,24 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '3tier-db-schema',
+        'USER': 'admin',
+        'PASSWORD': 'guoying1994322',
+        'HOST': 'database-3tier.c5y2vkxnuuax.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
